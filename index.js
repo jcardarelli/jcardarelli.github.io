19b40ba5cb51d78a31a22a85404e4b99ad573a6d
John Cardarelli <jcardarelli@users.noreply.github.com>
2024-06-01 22:41:38 -0700

    Add modal for fancy-cli

---
+++ index.js
@@ -0,0 +1,32 @@
+function openModalWindow() {
+  document
+    .getElementsByClassName("build")
+    .item(0)
+    .addEventListener("click", function () {
+      const modalContentId = document.getElementById("modal-content");
+      if (modalContentId.childNodes.length > 0) {
+        const dialogIsOpen = modalContentId.getElementsByTagName("dialog");
+        console.log("dialogIsOpen", dialogIsOpen);
+        dialogIsOpen.item(0).toggleAttribute("open", "true");
+      }
+    });
+}
+
+function closeModalWindow() {
+  document
+    .getElementsByTagName("dialog")
+    .item(0)
+    .addEventListener("click", function () {
+      const modalContentIdClosed = document.getElementById("modal-content");
+      if (modalContentIdClosed.childNodes.length > 0) {
+        const dialogIsClosed =
+          modalContentIdClosed.getElementsByTagName("dialog");
+        dialogIsClosed.item(0).removeAttribute("open");
+      }
+    });
+}
+
+window.onload = function () {
+  openModalWindow();
+  closeModalWindow();
+};
